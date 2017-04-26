2b7e4e6878a13f6b712465010b5717f1d75c311f
Marcos Paliari <marcos@paliari.com.br>
2017-04-26 12:00:16 -0300

    add src/v-autocomplete.js

---
+++ src/v-autocomplete.js
@@ -0,0 +1,6 @@
+// need to use require instead of import (changes from babel 5 to 6)
+const Autocomplete = require('./Autocomplete.vue')
+Autocomplete.install = Vue => Vue.component(Autocomplete.name, Autocomplete)
+Autocomplete.version = proccess.env.VERSION
+
+module.exports = Autocomplete
